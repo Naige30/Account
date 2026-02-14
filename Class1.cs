44042352e740e4effcaa8112ebc0784f42437089
Naige30 <ishigamisenku0603@gmail.com>
2026-02-14 12:29:19 +0800

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -1,7 +1,28 @@
-﻿namespace Account
+namespace Account
 {
     public class Class1
     {
+        static void main(String[] args)
+        {
+            Console.WriteLine("ACCOUNT MANAGEMENT SYSTEM");
+            string username="Naige123";
+            string password="Amarantheia123";
 
+            Console.WriteLine("Username: ");
+            string usernameINP = Console.ReadLine();
+            Console.WriteLine("Password: ");
+            string passwordINP = Console.ReadLine();
+
+            if(usernameINP ==username && passwordINP == password)
+            {
+                Console.WriteLine("Login Succesful");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect Credentials");
+                Console.WriteLine("Test");
+            }
+
+        }
     }
 }
